ceb88b0e5775af5c33143a84fc3dc67f83e08016
Platov Sergei <serg1764@gmail.com>
2023-11-10 21:56:30 +0300

    lesson - 49 Маршрутизация

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ import express from 'express';
 const port = '8000';
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.end('Привет!');
+app.all('/hello', (
+    req,
+    res,
+    next
+) => {
+    console.log('All');
+    next();
+});
+
+const cb = (req, res, next) => {
+    console.log('CB');
+    next();
+}
+app.get('/hello', cb, (req, res) => {
+    res.end('Привет! GET');
+});
+
+app.post('/hello', cb, (req, res) => {
+    res.end('Привет! POST');
 });
 
 app.listen(port, () => {
